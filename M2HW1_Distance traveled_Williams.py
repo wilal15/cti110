af60e321912824a3131095092661a8a6fde8d6e6
wilal15 <williama7058@student.faytechcc.edu>
2017-06-14 11:51:03 -0400

    Add files via upload

---
+++ M2HW1_Distance traveled_Williams.py
@@ -0,0 +1,12 @@
+# M2HW1_DistanceTraveled_Williams.py
+# Distance traveled
+# 6-9-2017
+#CTI-110 M2HW1 - Distance Traveled
+# Al Williams
+
+speed = 70
+print ('The distance a car will travel in 6 hours', speed * 6, 'miles')
+       
+print (' The distance a car will travel in 10 hours' , speed * 10, 'miles')
+print (' The distance a car will travel in 15 hours' , speed * 15, 'miles')
+
